265e71f7a271b5257a5436a96671222bb55b1e5e
Dave <TheBeaNerd@gmail.com>
2019-05-07 21:51:17 -0500

    updates build

--- linear2.lisp
+++ linear2.lisp
@@ -74,48 +74,35 @@
 
 (in-theory (disable (reconstruct-partial)))
 
-(def::un wf-basis-list (bases)
-  (declare (type t bases)
-           (xargs :congruence ((basis-list-equiv) equal)))
-  (if (not (consp bases)) t
-    (let ((basis (basis-fix! (car bases))))
-      (and (not (zero-polyp (basis->base basis)))
-           (equal (len (basis->coeffs basis)) (len (cdr bases)))
-           (all-zero (dot-list (basis->base basis)
-                               (basis-list-bases (cdr bases))))
-           (poly-equiv (basis->poly basis)
-                       (add (basis->base basis)
-                            (reconstruct-partial  
-                             (basis->coeffs basis) 
-                             (basis-list-fix (cdr bases)))))
-           (wf-basis-list (cdr bases))))))
+(def::un wf-basis-p (basis)
+  (declare (type t basis))
+  (and (basis-p basis)
+       (not (zero-polyp (basis->base basis)))))
 
-(def::un scale-coefficients (coeff clist)
-  (declare (xargs :signature ((rationalp rational-listp) rational-listp)))
-  (if (not (consp clist)) nil
-    (let ((c (car clist)))
-      (cons (* (rfix coeff) (rfix c))
-            (scale-coefficients coeff (cdr clist))))))
+(def::un wf-basis-entry (basis bases)
+  (declare (type t basis bases))
+  (and (wf-basis-p basis)
+       (equal (len (basis->coeffs basis)) (len bases))
+       (all-zero (dot-list (basis->base basis)
+                           (basis-list-bases bases)))
+       (poly-equiv (basis->poly basis)
+                   (add (basis->base basis)
+                        (reconstruct-partial  
+                         (basis->coeffs basis) 
+                         (basis-list-fix bases))))))
 
-(def::signature scale-coefficients (t t) rational-listp)
+(def::un wf-basis-listp (bases)
+  (declare (type t bases))
+  (if (not (consp bases)) (null bases)
+    (let ((basis (car bases)))
+      (and (wf-basis-entry basis (cdr bases))
+           (wf-basis-listp (cdr bases))))))
 
-(defthm len-scale-coefficients
-  (equal (len (scale-coefficients coeff clist))
-         (len clist)))
-
-(def::un add-coefficients (clist1 clist2)
-  (declare (xargs :signature ((rational-listp rational-listp) rational-listp)))
-  (if (not (and (consp clist1) (consp clist2))) nil
-    (let ((c1 (car clist1))
-          (c2 (car clist2)))
-      (cons (+ (rfix c1) (rfix c2))
-            (add-coefficients (cdr clist1) (cdr clist2))))))
-
-(def::signature add-coefficients (t t) rational-listp)
-
-(defthm len-add-coefficients
-  (equal (len (add-coefficients clist1 clist2))
-         (min (len clist1) (len clist2))))
+(defthm wf-basis-listp-implies-basis-listp
+  (implies
+   (wf-basis-listp list)
+   (basis-listp list))
+  :rule-classes (:forward-chaining))
 
 ;; (def::un repair-residual (residual coeffs bases)
 ;;   (declare (xargs :signature ((poly-p rational-listp basis-listp) poly-p)
@@ -188,7 +175,7 @@
 
 (defthm reconstruct-partial-poly-representatation
   (implies
-   (wf-basis-list bases)
+   (wf-basis-listp bases)
    (poly-equiv (add (val 0 (poly-representation poly bases)) 
                     (reconstruct-partial (val 1 (poly-representation poly bases)) bases))
                poly)))
@@ -220,7 +207,7 @@
 (defthm dot-base-reconstruct-partial-polys
   (implies
    (and
-    (wf-basis-list bases)
+    (wf-basis-listp bases)
     (all-zero (dot-list base (basis-list-polys bases))))
    (equal (dot base (reconstruct-partial coeffs bases)) 0)))
 
@@ -230,14 +217,16 @@
   (implies
    (and
     (all-zero (dot-list base (basis-list-bases bases)))
-    (wf-basis-list bases))
+    (wf-basis-listp bases))
    (all-zero (dot-list base (basis-list-polys bases))))
   :rule-classes (:rewrite :forward-chaining)
   :hints ((rewrite-equiv-hint poly-equiv)))
 
 (defthm dot-disjoint-base-poly-representation
   (implies
-   (all-zero (dot-list base (basis-list-polys bases)))
+   (and
+    (syntaxp (not (equal base `(val (quote 0) (poly-representation ,poly ,bases)))))
+    (all-zero (dot-list base (basis-list-polys bases))))
    (equal (dot base (val 0 (poly-representation poly bases)))
           (dot base poly))))
 
@@ -348,7 +337,7 @@
   (implies
    (and
     (POLY-EQUIV poly (ADD base (RECONSTRUCT-PARTIAL coeffs bases)))
-    (wf-basis-list bases)
+    (wf-basis-listp bases)
     (all-zero (dot-list base (basis-list-bases bases))))
    (equal (coeff poly base) 
           (if (or (zero-polyp poly) (zero-polyp base)) 0 1)))
@@ -363,7 +352,7 @@
 
 (defthm val0-poly-representation-reconstruct-partial
   (implies
-   (wf-basis-list bases)
+   (wf-basis-listp bases)
    (poly-equiv (val 0 (poly-representation (reconstruct-partial coeffs bases) bases))
                (zero-poly)))
   :hints (("Goal" :induct (reconstruct-partial coeffs bases)
@@ -388,7 +377,7 @@
 
 (defthm all-zero-dot-list-base-poly-representation
   (implies
-   (wf-basis-list bases)
+   (wf-basis-listp bases)
    (all-zero (dot-list (val 0 (poly-representation poly bases))
                        (basis-list-bases bases))))
   :rule-classes (:rewrite (:forward-chaining :trigger-terms ((poly-representation poly bases))))
@@ -397,3 +386,172 @@
                            (EQUAL-RFIX-TO-RFIX-EQUIV)))
           (rewrite-equiv-hint poly-equiv)))
 
+(def::und add-poly-to-bases (poly bases)
+  (declare (xargs :signature ((poly-p wf-basis-listp) wf-basis-listp)))
+  (metlist ((base coeffs) (poly-representation poly bases))
+    (if (zero-polyp base) bases
+      (cons (basis base coeffs poly) bases))))
+
+(defthm non-zero-dot-implies-non-zero-poly
+  (implies
+   (not (equal (dot x y) 0))
+   (and (not (zero-polyp x))
+        (not (zero-polyp y))))
+  :rule-classes (:forward-chaining))
+
+(encapsulate
+    ()
+
+  (local
+   (defthmd add-non-zero-poly-to-bases-helper
+     (implies
+      (and
+       (not (equal (dot base (val 0 (poly-representation poly bases))) 0))
+       (wf-basis-listp bases)
+       (all-zero (dot-list base (basis-list-bases bases))))
+      (equal (add-poly-to-bases poly bases)
+             (metlist ((base coeffs) (poly-representation poly bases))
+               (cons (basis base coeffs poly) bases))))
+     :hints (("Goal" :do-not-induct t :in-theory (e/d (add-poly-to-bases)
+                                                      (dot-disjoint-base-poly-representation))))))
+
+  (defthm add-non-zero-poly-to-bases
+    (implies
+     (and
+      (not (equal (dot base poly) 0))
+      (wf-basis-listp bases)
+      (all-zero (dot-list base (basis-list-bases bases))))
+     (equal (add-poly-to-bases poly bases)
+            (metlist ((base coeffs) (poly-representation poly bases))
+              (cons (basis base coeffs poly) bases))))
+    :hints (("Goal" :use add-non-zero-poly-to-bases-helper
+             :in-theory `(all-zero-dot-list-basis-list-polys
+                          dot-disjoint-base-poly-representation))))
+
+  )
+
+(def::un scale-coefficients (coeff clist)
+  (declare (xargs :signature ((rationalp rational-listp) rational-listp)))
+  (if (not (consp clist)) nil
+    (let ((c (car clist)))
+      (cons (* (rfix coeff) (rfix c))
+            (scale-coefficients coeff (cdr clist))))))
+
+(def::signature scale-coefficients (t t) rational-listp)
+
+(defthm len-scale-coefficients
+  (equal (len (scale-coefficients coeff clist))
+         (len clist)))
+
+(def::un add-coefficients (clist1 clist2)
+  (declare (xargs :signature ((rational-listp rational-listp) rational-listp)))
+  (if (not (and (consp clist1) (consp clist2))) nil
+    (let ((c1 (car clist1))
+          (c2 (car clist2)))
+      (cons (+ (rfix c1) (rfix c2))
+            (add-coefficients (cdr clist1) (cdr clist2))))))
+
+(def::signature add-coefficients (t t) rational-listp)
+
+(defthm len-add-coefficients
+  (equal (len (add-coefficients clist1 clist2))
+         (min (len clist1) (len clist2))))
+
+(defthm scale-zero-poly
+  (implies
+   (zero-polyp zpoly)
+   (poly-equiv (scale zpoly m) zpoly)))
+
+(encapsulate
+    ()
+
+  (local
+   (defthmd non-zero-val-0-poly-representation-helper
+     (implies
+      (and
+       (not (equal (dot base poly) 0))
+       (wf-basis-listp bases)
+       (all-zero (dot-list base (basis-list-bases bases))))
+      (not (zero-polyp (val 0 (poly-representation poly bases)))))
+     :hints (("Goal" :in-theory (e/d (ZERO-POLYP-DEFINITION)
+                                     (POLY-EQUIV-IMPLIES-EQUAL-DOT-2))
+              :use (:instance poly-equiv-implication
+                              (x (val 0 (poly-representation poly bases)))
+                              (y (zero-poly))
+                              (k base))
+              :do-not-induct t))))
+
+  (defthm non-zero-val-0-poly-representation
+    (implies
+     (and
+      (< (coeff base poly) 0)
+      (wf-basis-listp bases)
+      (all-zero (dot-list base (basis-list-bases bases))))
+     (not (zero-polyp (val 0 (poly-representation poly bases)))))
+    :hints (("Goal" :use non-zero-val-0-poly-representation-helper)))
+
+  )
+
+;; (dot (add (basis->base basis) (scale base (- (coeff poly (basis->base basis))))) poly)
+
+(def::un update-fred (poly basis bases)
+  (declare (xargs :signature ((poly-p wf-basis-p wf-basis-listp) basis-p basis-listp)
+                  :signature-hints (("Goal" :do-not-induct t))))
+  (let ((coeff (coeff poly (basis->base basis))))
+    (if (<= 0 coeff) (mv basis bases)
+      (metlist ((base coeffs) (poly-representation poly bases))
+        ;; Technically just compensating for the magnitude of base
+        (let ((coeff (coeff (basis->base basis) base)))
+          (let ((basis->base  (add (basis->base basis) (scale base (- coeff))))
+                (basis->coeff (cons coeff (add-coefficients (basis->coeffs basis) (scale-coefficients (- coeff) coeffs))))
+                (basis->poly  (basis->poly basis)))
+            (if (zero-polyp basis->base) (mv basis bases)
+              (mv (basis basis->base basis->coeff basis->poly)
+                  (cons (basis base coeffs poly) bases)))))))))
+
+(defthm all-zero-dot-list-add-2
+  (implies (all-zero (dot-list base basis-list))
+           (iff (all-zero (dot-list (add base poly) basis-list))
+                (all-zero (dot-list poly basis-list))))
+  :hints (("goal" :in-theory (disable all-zero-dot-list-add)
+           :use all-zero-dot-list-add)))
+
+(defthm all-zero-scale-coefficients
+  (implies
+   (equal (rfix m) 0)
+   (all-zero (scale-coefficients m list))))
+
+(defthm all-all-zero-coefficients
+  (implies
+   (and (all-zero y) (force (equal (len x) (len y))))
+   (equal (add-coefficients x y)
+          (rational-list-fix x))))
+
+(defthm reconstruct-partial-add-coefficients
+  (implies
+   (force (equal (len x) (len y)))
+   (poly-equiv (reconstruct-partial (add-coefficients x y) bases)
+               (add (reconstruct-partial x bases)
+                    (reconstruct-partial y bases)))))
+
+(defthm reconstruct-partial-scale-coefficients
+  (poly-equiv (reconstruct-partial (scale-coefficients m list) bases)
+              (scale (reconstruct-partial list bases) m)))
+
+(defthm reconstruct-scaled-partial-poly-representatation
+  (implies
+   (wf-basis-listp bases)
+   (poly-equiv (add (scale (val 0 (poly-representation poly bases)) m)
+                    (scale (reconstruct-partial (val 1 (poly-representation poly bases)) bases) m))
+               (scale poly m))))
+
+(defthm wf-basis-entry-update-fred
+  (implies
+   (and
+    (wf-basis-listp bases)
+    (wf-basis-entry basis bases))
+   (wf-basis-entry (val 0 (update-fred poly basis bases))
+                   (val 1 (update-fred poly basis bases))))
+  :hints (("Goal" :in-theory (e/d (rfix-equiv)
+                                  (equal-rfix-TO-RFIX-EQUIV))
+           :do-not-induct t)))
